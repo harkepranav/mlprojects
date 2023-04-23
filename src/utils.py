67d35a0c4c61e5761787f51ddc5edbe43958ac5d
Pranav Harke <harkep20@outlook.com>
2023-04-23 21:17:43 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,10 @@
-# All common funcationalities that entire project can us
 import os
 import sys
-import dill as dill
+import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 
 from src.exception import CustomException
 
@@ -13,7 +12,7 @@ from src.exception import CustomException
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path, exist_ok= True)
+        os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
@@ -21,13 +20,23 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 # Function to evaluate models
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
+
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+
+            # model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
@@ -41,5 +50,3 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
 
     except Exception as e:
         raise CustomException(e, sys)
-
-
