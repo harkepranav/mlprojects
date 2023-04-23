4da5009c465988dbcff1e3d511ff97dee482db86
Pranav Harke <harkep20@outlook.com>
2023-04-23 12:25:58 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,13 +6,16 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
     # Trained data will be stored in this path
-    train_data_path: str= os.path.join('artifacts','train.csv')
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
     test_data_path: str = os.path.join('artifacts', 'test.csv')
     raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
 
 class DataIngestion:
     def __init__(self):
@@ -29,8 +32,8 @@ class DataIngestion:
 
             logging.info('Train test split initiated')
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
-            train_set.to_csv(self.ingestion_config.train_data_path, index= False, header= True)         # Saved train file
-            test_set.to_csv(self.ingestion_config.test_data_path, index= False, header= True)           # Saved test file
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)  # Saved train file
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)  # Saved test file
 
             logging.info('Data Ingestion has been completed')
 
@@ -42,6 +45,9 @@ class DataIngestion:
             logging.error(f"Exception occurred during data ingestion: {e}")
             raise CustomException("Failed to ingest data", str(e))
 
+
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data_path, test_data_path = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data_path, test_data_path)
